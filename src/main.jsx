08eaeaa82e46f6a903202e39a20c5969b48ee461
逸恒 <hengchang.lhc@alibaba-inc.com>
2023-07-07 15:48:39 +0800

    feat: update antd3

--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,14 @@ import ReactDOM from 'react-dom/client'
 // import App1 from './App1';
 // import App2 from './App2';
 // import App3 from './App3';
-import App4 from './App-antd4';
+import AppAntd3 from './App-antd3';
+// import App4 from './App-antd4';
 // import App4 from './App-antd4-css-variables';
 
 // import App5 from './App-antd5';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App4 />
+    <AppAntd3 />
   </React.StrictMode>,
 )
