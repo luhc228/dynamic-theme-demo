e01ff02179b27a07481456869185266e14a7ae69
luhc228 <luhengchang228@126.com>
2023-07-24 14:58:03 +0800

    feat: add antd3 dark theme

--- src/App-antd3/index.jsx
+++ src/App-antd3/index.jsx
@@ -1,16 +1,63 @@
 import './init';
-import { useState } from 'react';
-import { Button, Checkbox, Form, Input, ConfigProvider } from 'antd';
+import { useState, Component } from 'react';
+import { Button, Checkbox, Form, Input, ConfigProvider, Icon } from 'antd';
 import './index.css';
+import 'antd/dist/antd.css';
 
-const formItemLayout = {
-  labelCol: { span: 4 },
-  wrapperCol: { span: 8 },
-};
-const formTailLayout = {
-  labelCol: { span: 4 },
-  wrapperCol: { span: 8, offset: 4 },
-};
+class NormalLoginForm extends Component {
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
+  };
+
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    return (
+      <Form onSubmit={this.handleSubmit} className="login-form">
+        <Form.Item>
+          {getFieldDecorator('username', {
+            rules: [{ required: true, message: 'Please input your username!' }],
+          })(
+            <Input
+              prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              placeholder="Username"
+            />,
+          )}
+        </Form.Item>
+        <Form.Item>
+          {getFieldDecorator('password', {
+            rules: [{ required: true, message: 'Please input your Password!' }],
+          })(
+            <Input
+              prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              type="password"
+              placeholder="Password"
+            />,
+          )}
+        </Form.Item>
+        <Form.Item>
+          {getFieldDecorator('remember', {
+            valuePropName: 'checked',
+            initialValue: true,
+          })(<Checkbox>Remember me</Checkbox>)}
+          <a className="login-form-forgot" href="">
+            Forgot password
+          </a>
+          <Button type="primary" htmlType="submit" className="login-form-button">
+            Log in
+          </Button>
+          Or <a href="">register now!</a>
+        </Form.Item>
+      </Form>
+    );
+  }
+}
+
+const WrappedNormalLoginForm = Form.create({ name: 'normal_login' })(NormalLoginForm);
 
 export default function App4() {
   const [prefixCls, setPrefixCls] = useState('light');
@@ -23,25 +70,11 @@ export default function App4() {
           <Button onClick={() => setPrefixCls('light')}>Light</Button>
           <Button type='primary' onClick={() => setPrefixCls('dark')}>Dark</Button>
         </>
-        <div>
-          <Form.Item {...formItemLayout} label="Name">
-            <Input placeholder="Please input your name" />
-          </Form.Item>
-          <Form.Item {...formItemLayout} label="Nickname">
-            <Input placeholder="Please input your nickname" />
-          </Form.Item>
-          <Form.Item {...formTailLayout}>
-            <Checkbox onClick={() => { }}>
-              Nickname is required
-            </Checkbox>
-          </Form.Item>
-          <Form.Item {...formTailLayout}>
-            <Button type="primary" onClick={() => {}}>
-              Check
-            </Button>
-          </Form.Item>
-        </div>
+
+          <WrappedNormalLoginForm />
       </ConfigProvider>
+
+      {/* <WrappedNormalLoginForm /> */}
     </main>
   )
 }
